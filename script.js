0332d97d1b7980091261d3d3a80a17eb2f525bf9
joaolucas3002 <92446800+joaolucas3002@users.noreply.github.com>
2022-01-21 04:13:30 -0300

    .

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-let saibaMais = document.querySelector("#saiba-mais");
-saibaMais.onclick = function saiba(){
-    return{
-        from:document.querySelector("#from").style.display = "inline-block",
-        saibaMais:document.querySelector("#saiba-mais").style.display = "none",
-    }
-}
-let x = document.querySelector("#fechar");
-x.onclick = function saiba(){
-    return{
-        from:document.querySelector("#from").style.display = "none",
-        saibaMais:document.querySelector("#saiba-mais").style.display = "block",
-    }
-}
-
-
-let valor = document.querySelector("#button") 
-valor.onclick = function enviar() {
-        let text = document.querySelector("#text").value;
-        let mail =document.querySelector("#email").value;
-
-        return{
-            text:localStorage.getItem("name",text),
-            mail:localStorage.getItem("mail",mail),
-        }
-    }
-
 
 
 
